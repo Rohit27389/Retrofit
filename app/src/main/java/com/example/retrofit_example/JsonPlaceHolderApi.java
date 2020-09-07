9a7b8f90144cbbcae546d3016f868f252638c493
rohti kumar <rohit27389@gmail.com>
2020-09-07 19:40:06 +0530

    Retrofit

---
+++ app/src/main/java/com/example/retrofit_example/JsonPlaceHolderApi.java
@@ -0,0 +1,11 @@
+package com.example.retrofit_example;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface JsonPlaceHolderApi {
+    @GET("posts")
+    Call<List<Post>> getPosts();
+}
